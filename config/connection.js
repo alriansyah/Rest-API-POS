d1614a80ee5bae3eff0e9e2704f2bbb7caaec19f
alriansyah <alriansyah1218@gmail.com>
2023-05-07 00:08:34 +0700

    Buat env dan setup sebelum mulai rest api

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,19 @@
 const mysql = require('mysql');
+const env = require('dotenv');
 
+env.config();
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'pos_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
+connection.connect((err) => {
+    if(err) {
+        console.log("Error", err);
+    }
+    console.log('connect');
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const db = require('./config/connection')
 const port = 3000
 
+// db.connect();
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
+    console.log(process.env.HOSTNAME);
     res.send('Hello Al Riansyah!')
 })
+
+app.post('/product', (req, res) => {
+    console.log(req.body);
+    res.send('testing');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
